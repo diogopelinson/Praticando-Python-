f8da4771fb54b353aaa8147a86e3c0238f1680ba
diogopelinson <diogopelinsonduartemoraes@gmail.com>
2025-03-20 00:10:33 -0300

    Exercicios de if,elif e else, terminados

--- ex9.py
+++ ex9.py
@@ -2,4 +2,4 @@ numero = int(input("Digite um numero inteiro: "))
 if numero % 2 == 0:
     print("Seu numero é par ")
 elif numero % 2 != 0:
-    print("Seu número é ímpar")
+    print("Seu número é ímpar") 
